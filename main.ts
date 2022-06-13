2eb6159c2f6d37387c576160c8ab0f6cb1877cab
PauPino11 <106530191+PauPino11@users.noreply.github.com>
2022-06-13 13:26:29 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,40 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . 1 1 1 1 . . . . . . . 
+        . . . . . 1 . 3 3 3 . 1 . . . . 
+        . . . . . . 3 3 3 3 3 1 . . . . 
+        . . . . . 3 3 3 3 3 3 3 . . . . 
+        . . . . . 3 3 3 3 3 3 3 . . . . 
+        . . . . . . . 3 b 3 . . . . . . 
+        . . . . . . . 3 b 3 1 . . . . . 
+        . . . . . . . 3 3 3 1 . . . . . 
+        . . . . . . . 3 3 3 1 . . . . . 
+        . . . . . . . 3 3 3 . . . . . . 
+        . . . . . 3 3 3 3 3 3 3 . . . . 
+        . . . . . 3 b b 3 3 b 3 . . . . 
+        . . . . . 3 b 3 3 3 b 3 . . . . 
+        . . . . . 3 3 3 3 3 3 3 . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, OTHER_SPRITE, 0, -50)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    ASTEROID.destroy()
+    OTHER_SPRITE.destroy(effects.confetti, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    OTHER_SPRITE.destroy(effects.fire, 500)
+    scene.cameraShake(4, 500)
+})
 let ASTEROID: Sprite = null
+let projectile: Sprite = null
+let OTHER_SPRITE: Sprite = null
 game.splash("BENVINGUTS A L'ESPAI", "SI VOLS SOBREVIURE MATA ELS")
 game.splash("CLI A PER COMENÇAR I B PER DISPARAR")
-let mySprite = sprites.create(img`
+OTHER_SPRITE = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . 8 . . . . . . . . 
     . . . . . . 6 1 6 . . . . . . . 
@@ -19,9 +52,9 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(80, 50)
-controller.moveSprite(mySprite, 100, 100)
-mySprite.setStayInScreen(true)
+OTHER_SPRITE.setPosition(80, 50)
+controller.moveSprite(OTHER_SPRITE, 100, 100)
+OTHER_SPRITE.setStayInScreen(true)
 info.setLife(4)
 game.onUpdate(function () {
     ASTEROID = sprites.createProjectileFromSide(img`
@@ -41,6 +74,7 @@ game.onUpdate(function () {
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, 50, 50)
+        `, 0, 50)
     ASTEROID.x += randint(0, scene.screenWidth())
+    ASTEROID.setKind(SpriteKind.Enemy)
 })
